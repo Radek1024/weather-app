91a85cd760ebbc627e22e7f0a2789aaa0c58326e
Radek <radoslaw.paszkiewicz1024@gmail.com>
2021-03-19 10:06:10 +0100

    updating

--- src/main/java/com/weather/forecast/ForecastApplication.java
+++ src/main/java/com/weather/forecast/ForecastApplication.java
@@ -1,23 +1,26 @@
 package com.weather.forecast;
 
 import com.weather.forecast.utils.ApiConfig;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
-//@ConfigurationPropertiesScan("com.weather.forecast.utils")
 @EnableConfigurationProperties(ApiConfig.class)
 public class ForecastApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ForecastApplication.class, args);
     }
+
     @Bean
     public RestTemplate restTemplate(){
         return new RestTemplate();
     }
+
 }
