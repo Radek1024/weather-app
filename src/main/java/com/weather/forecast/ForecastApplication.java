a1175d102a306461c68edd60552d4607a1a51131
Radek <radoslaw.paszkiewicz1024@gmail.com>
2021-03-31 20:52:25 +0200

    looking good

--- src/main/java/com/weather/forecast/ForecastApplication.java
+++ src/main/java/com/weather/forecast/ForecastApplication.java
@@ -3,12 +3,13 @@ package com.weather.forecast;
 import com.weather.forecast.utils.ApiConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
-@EnableConfigurationProperties(ApiConfig.class)
+@ConfigurationPropertiesScan("com.weather.forecast.utils")
 public class ForecastApplication {
 
     public static void main(String[] args) {
